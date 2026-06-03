eea554696954ed53337af24e76365c3300b13b5f
dh0250 <hyun02549@gmail.com>
2026-06-04 00:30:00 +0900

    rest-api

--- rest-api/src/main/java/com/ohgiraffers/restapi/dto/ErrorResponse.java
+++ rest-api/src/main/java/com/ohgiraffers/restapi/dto/ErrorResponse.java
@@ -1,5 +1,6 @@
 package com.ohgiraffers.restapi.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,11 +17,8 @@ public class ErrorResponse {
     private Integer status;
     private String errorCode;
     private String message;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private Map<String, String> errors;
 
-    public ErrorResponse(Integer status, String errorCode, String message) {
-        this.status = status;
-        this.errorCode = errorCode;
-        this.message = message;
-    }
 }
